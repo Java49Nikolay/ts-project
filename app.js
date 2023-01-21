bb3d4b24bde944e188ce0c2a93d901a62d2d2c9c
Nik.choporev <nik.choporev@gmail.com>
2023-01-21 14:20:27 +0200

    HW #32 Fix getEmployee

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 "use strict";
-// for (let i = 0; i < 3; i++) {
-//     setTimeout(()=> console.log(i));
+Object.defineProperty(exports, "__esModule", { value: true });
+const Company_1 = require("./Company");
+const SalesPerson_1 = require("./SalesPerson");
+const WageEmployee_1 = require("./WageEmployee");
+const employees = [new WageEmployee_1.WageEmployee(123, "Vasya", 1970, "Development", 10000, 100, 100), new WageEmployee_1.WageEmployee(124, "Sara", 1975, "Management", 12000, 100, 100), new SalesPerson_1.SalesPerson(125, "Abraham", 1980, "Sales", 11000, 100, 100, 10000, 1)];
+const company = new Company_1.Company(employees);
+console.log(`salary budget is ${company.computeBudget()}`);
+const empl1 = new SalesPerson_1.SalesPerson(127, "Victor", 2000, "QA", 10000, 100, 100, 1000, 1);
+const salary1 = empl1.computeSalary();
+company.addEmployee(empl1);
+console.log(`after adding new employee with salary ${salary1} the salary budget is ${company.computeBudget()} `);
+const empl2 = company.getEmployee(125);
+// if (empl2) {
+//    const salary2 = empl2.computeSalary();
 // }
-// let a = 10
-// console.log(`a = ${a}`)
-//vaiables
-let hello = "world";
-const vasya = "Vasya";
-let b;
-b = 10;
-let c;
-c = "10";
-function getRadomNumber(min, max) {
-    return min + Math.floor(min + Math.random() * (max - min + 1));
-}
+//const salary2 = empl2!.computeSalary();//programmer knows for sure that null cannot be
+const salary2 = empl2.computeSalary();
+company.removeEmployee(125);
+console.log(`after removing  employee with salary ${salary2} the salary budget is ${company.computeBudget()} `);
+//# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,16 +1,22 @@
-// for (let i = 0; i < 3; i++) {
-//     setTimeout(()=> console.log(i));
+import { Company } from "./Company";
+import { Employee } from "./Employee"
+import { SalesPerson } from "./SalesPerson"
+import { WageEmployee } from "./WageEmployee"
+const employees: Array<Employee> = [new WageEmployee(123, "Vasya", 1970,
+ "Development", 10000, 100, 100), new WageEmployee(124, "Sara", 1975, "Management",
+  12000, 100, 100), new SalesPerson(125, "Abraham", 1980, "Sales", 11000, 100,
+   100, 10000, 1)];
+const company = new Company(employees);
+console.log(`salary budget is ${company.computeBudget()}`);
+const empl1 = new SalesPerson(127, "Victor",2000, "QA",10000, 100, 100,1000,1);
+const salary1 = empl1.computeSalary();
+company.addEmployee(empl1);
+console.log(`after adding new employee with salary ${salary1} the salary budget is ${company.computeBudget()} `)
+const empl2: Employee|null = company.getEmployee(125);
+// if (empl2) {
+//    const salary2 = empl2.computeSalary();
 // }
-// let a = 10
-// console.log(`a = ${a}`)
-
-//vaiables
-let hello = "world";
-const vasya = "Vasya";
-let b;
-b = 10;
-let c: string;
-c = "10";
-function getRadomNumber(min:number,max:number) :number {
-     return min + Math.floor(min + Math.random() * (max - min + 1));
-}
+//const salary2 = empl2!.computeSalary();//programmer knows for sure that null cannot be
+const salary2: number = empl2!.computeSalary();
+company.removeEmployee(125);
+console.log(`after removing  employee with salary ${salary2} the salary budget is ${company.computeBudget()} `)
